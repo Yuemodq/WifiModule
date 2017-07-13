8fb45ea0b112539d340453aa6d017a52d057c36a
origin <xw_appdev@163.com>
2017-07-14 01:35:13 +0800

    添加.gitignore文件

---
+++ app/src/main/java/com/xw/wifimodule/activitty/MainActivity.java
@@ -0,0 +1,51 @@
+package com.xw.wifimodule.activitty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xw.wifimodule.R;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
